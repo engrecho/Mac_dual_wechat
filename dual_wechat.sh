f0d51c9975719413b3928e60b8cd770721c23803
Jaylon <engrecho@foxmail.com>
2025-09-21 18:57:23 +0800

    Update dual_wechat.sh

--- dual_wechat.sh
+++ dual_wechat.sh
@@ -1,9 +1,5 @@
 sudo cp -R /Applications/WeChat.app /Applications/WeChat2.app​
 
-curl -o https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/10809935e3624d1183fa5bcb89c94345_ChatWise.icns
-
-sudo mv 10809935e3624d1183fa5bcb89c94345_ChatWise.icns /Applications/WeChat2.app/Contents/Resources/AppIcon.icns​
-
 sudo /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier com.tencent.xinWeChat2" /Applications/WeChat2.app/Contents/Info.plist​
 
 sudo codesign --force --deep --sign - /Applications/WeChat2.app
